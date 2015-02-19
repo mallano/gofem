b2864ef722886a09548ffb56cb0a51f07f7c6bea
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-02-19 18:03:00 +1000

    main and tools fixed

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"flag"
 
 	"github.com/cpmech/gofem/fem"
+	"github.com/cpmech/gosl/chk"
+	"github.com/cpmech/gosl/io"
 	"github.com/cpmech/gosl/mpi"
 	"github.com/cpmech/gosl/utl"
 )
@@ -15,11 +17,10 @@ import (
 func main() {
 
 	// catch errors
-	utl.Tsilent = false
 	defer func() {
 		if mpi.Rank() == 0 {
 			if err := recover(); err != nil {
-				utl.PfRed("ERROR: %v\n", err)
+				io.PfRed("ERROR: %v\n", err)
 			}
 		}
 		mpi.Stop(false)
@@ -27,10 +28,10 @@ func main() {
 	mpi.Start(false)
 
 	// message
-	utl.PfWhite("\nGofem v3 -- Go Finite Element Method\n\n")
-	utl.Pf("Copyright 2012 Dorival Pedroso & Raul Durand. All rights reserved.\n")
-	utl.Pf("Use of this source code is governed by a BSD-style\n")
-	utl.Pf("license that can be found in the LICENSE file.\n\n")
+	io.PfWhite("\nGofem v3 -- Go Finite Element Method\n\n")
+	io.Pf("Copyright 2015 Dorival Pedroso & Raul Durand. All rights reserved.\n")
+	io.Pf("Use of this source code is governed by a BSD-style\n")
+	io.Pf("license that can be found in the LICENSE file.\n\n")
 
 	// simulation filenamepath
 	flag.Parse()
@@ -38,17 +39,17 @@ func main() {
 	if len(flag.Args()) > 0 {
 		fnamepath = flag.Arg(0)
 	} else {
-		utl.Panic("Please, provide a filename. Ex.: cylinder.sim")
+		chk.Panic("Please, provide a filename. Ex.: cylinder.sim")
 	}
 
 	// other options
 	erasefiles := true
 	verbose := true
 	if len(flag.Args()) > 1 {
-		erasefiles = utl.Atob(flag.Arg(1))
+		erasefiles = io.Atob(flag.Arg(1))
 	}
 	if len(flag.Args()) > 2 {
-		verbose = utl.Atob(flag.Arg(2))
+		verbose = io.Atob(flag.Arg(2))
 	}
 
 	// profiling?
@@ -56,7 +57,7 @@ func main() {
 
 	// start global variables and log
 	if !fem.Start(fnamepath, erasefiles, verbose) {
-		utl.Panic("Start failed\n")
+		chk.Panic("Start failed\n")
 		return
 	}
 
@@ -65,7 +66,7 @@ func main() {
 
 	// run simulation
 	if !fem.Run() {
-		utl.Panic("Run failed\n")
+		chk.Panic("Run failed\n")
 		return
 	}
 }
--- tools/ConvertGofemMat.go
+++ tools/ConvertGofemMat.go
@@ -9,18 +9,10 @@ import (
 
 	"github.com/cpmech/gofem/inp"
 
-	"github.com/cpmech/gosl/utl"
+	"github.com/cpmech/gosl/io"
 )
 
 func main() {
-
-	// error handler
-	utl.Tsilent = false
-	defer func() {
-		if err := recover(); err != nil {
-			utl.PfRed("Some error has happened: %v\n", err)
-		}
-	}()
 
 	// input data
 	matOld := "matOld.mat"
@@ -34,18 +26,18 @@ func main() {
 		matNew = flag.Arg(1)
 	}
 	if len(flag.Args()) > 2 {
-		convSymb = utl.Atob(flag.Arg(2))
+		convSymb = io.Atob(flag.Arg(2))
 	}
 
 	// print input data
-	utl.Pf("\nInput data\n")
-	utl.Pf("==========\n")
-	utl.Pf("  matOld   = %20s // old material filename\n", matOld)
-	utl.Pf("  matNew   = %20s // new material filename\n", matNew)
-	utl.Pf("  convSymb = %20v // do convert symbols\n", convSymb)
-	utl.Pf("\n")
+	io.Pf("\nInput data\n")
+	io.Pf("==========\n")
+	io.Pf("  matOld   = %20s // old material filename\n", matOld)
+	io.Pf("  matNew   = %20s // new material filename\n", matNew)
+	io.Pf("  convSymb = %20v // do convert symbols\n", convSymb)
+	io.Pf("\n")
 
 	// convert old => new
 	inp.MatfileOld2New("", matNew, matOld, convSymb)
-	utl.Pfblue2("conversion successful\n")
+	io.Pfblue2("conversion successful\n")
 }
