ee116d63cbd8ac094408ddd59d8f2f6c3a36d6c8
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-02-20 10:41:34 +1000

    copyright message fixed

--- fixnewpkgs.bash
+++ fixnewpkgs.bash
@@ -61,9 +61,9 @@ fix_pkgs_simple() {
     cd $PKG
 
     for f in *.go; do
-        #sed -i -e 's/Copyright 2012 Dorival de Moraes Pedroso/Copyright 2015 Dorival Pedroso and Raul Durand/g' $f
+        sed -i -e 's/Copyright 2012 Dorival de Moraes Pedroso/Copyright 2015 Dorival Pedroso and Raul Durand/g' $f
         #sed -i -e 's/Copyright 2012 Dorival Pedroso & Raul Durand/Copyright 2015 Dorival Pedroso and Raul Durand/g' $f
-        sed -i -e 's/Copyright 2015 Dorival Pedroso & Raul Durand/Copyright 2015 Dorival Pedroso and Raul Durand/g' $f
+        #sed -i -e 's/Copyright 2015 Dorival Pedroso & Raul Durand/Copyright 2015 Dorival Pedroso and Raul Durand/g' $f
         #goimports -w $f
     done
 
