1360ff8788558edb7942166caf2632335908f905
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-02-20 12:50:21 +1000

    m1 conduct parameters renamed

--- mconduct/m1.go
+++ mconduct/m1.go
@@ -13,16 +13,16 @@ import (
 type M1 struct {
 
 	// parameters for liquid
-	λ0L float64
-	λ1L float64
-	αL  float64
-	βL  float64
+	λ0l float64
+	λ1l float64
+	αl  float64
+	βl  float64
 
 	// parameters for gas
-	λ0G float64
-	λ1G float64
-	αG  float64
-	βG  float64
+	λ0g float64
+	λ1g float64
+	αg  float64
+	βg  float64
 
 	// auxiliary functions
 	klr fun.RefIncRL1
@@ -37,14 +37,14 @@ func init() {
 // GetPrms gets (an example) of parameters
 func (o M1) GetPrms(example bool) fun.Prms {
 	return fun.Prms{
-		&fun.Prm{N: "lam0L", V: 0.001},
-		&fun.Prm{N: "lam1L", V: 1.2},
-		&fun.Prm{N: "alpL", V: 0.01},
-		&fun.Prm{N: "betL", V: 10},
-		&fun.Prm{N: "lam0G", V: 2.0},
-		&fun.Prm{N: "lam1G", V: 0.001},
-		&fun.Prm{N: "alpG", V: 0.01},
-		&fun.Prm{N: "betG", V: 10},
+		&fun.Prm{N: "lam0l", V: 0.001},
+		&fun.Prm{N: "lam1l", V: 1.2},
+		&fun.Prm{N: "alpl", V: 0.01},
+		&fun.Prm{N: "betl", V: 10},
+		&fun.Prm{N: "lam0g", V: 2.0},
+		&fun.Prm{N: "lam1g", V: 0.001},
+		&fun.Prm{N: "alpg", V: 0.01},
+		&fun.Prm{N: "betg", V: 10},
 	}
 }
 
@@ -52,37 +52,37 @@ func (o M1) GetPrms(example bool) fun.Prms {
 func (o *M1) Init(prms fun.Prms) (err error) {
 	for _, p := range prms {
 		switch p.N {
-		case "lam0L":
-			o.λ0L = p.V
-		case "lam1L":
-			o.λ1L = p.V
-		case "alpL":
-			o.αL = p.V
-		case "betL":
-			o.βL = p.V
-		case "lam0G":
-			o.λ0G = p.V
-		case "lam1G":
-			o.λ1G = p.V
-		case "alpG":
-			o.αG = p.V
-		case "betG":
-			o.βG = p.V
+		case "lam0l":
+			o.λ0l = p.V
+		case "lam1l":
+			o.λ1l = p.V
+		case "alpl":
+			o.αl = p.V
+		case "betl":
+			o.βl = p.V
+		case "lam0g":
+			o.λ0g = p.V
+		case "lam1g":
+			o.λ1g = p.V
+		case "alpg":
+			o.αg = p.V
+		case "betg":
+			o.βg = p.V
 		default:
 			return chk.Err("mconduct.M1: parameter named %q is incorrect\n", p.N)
 		}
 	}
 	o.klr.Init(fun.Prms{
-		&fun.Prm{N: "lam0", V: o.λ0L},
-		&fun.Prm{N: "lam1", V: o.λ1L},
-		&fun.Prm{N: "alp", V: o.αL},
-		&fun.Prm{N: "bet", V: o.βL},
+		&fun.Prm{N: "lam0", V: o.λ0l},
+		&fun.Prm{N: "lam1", V: o.λ1l},
+		&fun.Prm{N: "alp", V: o.αl},
+		&fun.Prm{N: "bet", V: o.βl},
 	})
 	o.kgr.Init(fun.Prms{
-		&fun.Prm{N: "lam0", V: o.λ0G},
-		&fun.Prm{N: "lam1", V: o.λ1G},
-		&fun.Prm{N: "alp", V: o.αG},
-		&fun.Prm{N: "bet", V: o.βG},
+		&fun.Prm{N: "lam0", V: o.λ0g},
+		&fun.Prm{N: "lam1", V: o.λ1g},
+		&fun.Prm{N: "alp", V: o.αg},
+		&fun.Prm{N: "bet", V: o.βg},
 	})
 	return
 }
