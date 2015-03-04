9882a403fa5bf28e41d0a16ae123df923871a4ff
Dorival Pedroso <dorival.pedroso@gmail.com>
2015-03-05 09:29:06 +1000

    examples' files fixed

--- examples/all.bash
+++ examples/all.bash
@@ -2,9 +2,12 @@
 
 examples="rjoint_ex06_pullout \
           seep_ex01_freesurf \
+          seep_ex02_freesurf \
           spo751_pressurised_cylinder \
           spo754_strip_footing_collapse \
-          up_3mcolumn_desiccation"
+          up_3mcolumn_desiccation \
+          up_indentation2d_unsat"
+
 for ex in $examples; do
     echo
     echo
